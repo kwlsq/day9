7e9667caca276776424dbf6e8356d3c8872683c1
Vincentius <vincentiussss@gmail.com>
2025-02-13 13:36:57 +0700

    feat(user): init user class

---
+++ src/main/java/org/vincent/entity/User.java
@@ -0,0 +1,13 @@
+package org.vincent.entity;
+
+public class User {
+    private String userName;
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
+
+
